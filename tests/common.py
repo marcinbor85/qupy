c1703252de1d0a70128acc58f5d4da49d0763642
Marcin Borowicz <marcinbor85@gmail.com>
2020-07-16 19:17:58 +0200

    tests refactored

---
+++ tests/common.py
@@ -0,0 +1,25 @@
+import queue
+
+from qupy.interface import AbstractInterface
+from qupy.interface.errors import InterfaceTimeoutError
+
+class EchoInterface(AbstractInterface):
+    enabled = True
+
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def read(self):
+        if not self.enabled:
+            raise InterfaceTimeoutError()
+
+        try:
+            rx_char = self.queue.get_nowait()    
+        except queue.Empty:
+            raise InterfaceTimeoutError()
+    
+        return bytes([rx_char])
+
+    def write(self, bytes_buf):
+        for b in bytes_buf:
+            self.queue.put(b)
--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,26 +1,11 @@
 import unittest
 
 from qupy.framing.slip import Slip
-from qupy.core import CommWorker
-from qupy.interface import AbstractInterface
-from qupy.errors import CommTimeoutError
 
+from qupy.comm.client import CommClient
+from qupy.comm.errors import CommTimeoutError
 
-class EchoInterface(AbstractInterface):
-    offset = 0
-    data = bytearray()
-    enabled = True
-
-    def read(self):
-        if self.enabled and self.offset < len(self.data):
-            byte = self.data[self.offset:self.offset+1]
-            self.offset += 1
-            return byte
-        return None
-    
-    def write(self, bytes_buf):
-        self.data = bytes_buf
-        self.offset = 0
+from .common import EchoInterface
 
 
 class TestSlipFraming(unittest.TestCase):
@@ -28,7 +13,7 @@ class TestSlipFraming(unittest.TestCase):
     def setUp(self):
         self.framing = Slip()
         self.interface = EchoInterface()
-        self.comm = CommWorker(self.interface, self.framing)
+        self.comm = CommClient(self.interface, self.framing)
         self.comm.start()
 
     def test_send_and_recv(self):
@@ -36,27 +21,38 @@ class TestSlipFraming(unittest.TestCase):
         tx_data = b'\x01\x02\x03'
         self.comm.send(tx_data)
         rx_data = self.comm.recv()
-        self.assertEqual(self.interface.data, self.framing.encode_frame(tx_data))
         self.assertEqual(rx_data, tx_data)
     
     def test_send_recv(self):
         self.interface.enabled = True
         tx_data = b'\x01\x02\x03'
         rx_data = self.comm.send_recv(tx_data)
-        self.assertEqual(self.interface.data, self.framing.encode_frame(tx_data))
         self.assertEqual(rx_data, tx_data)
     
-    def test_recv(self):
+    def test_recv_timeout(self):
         self.interface.enabled = False
         tx_data = b'\x01\x02\x03'
         with self.assertRaises(CommTimeoutError):
-            rx_data = self.comm.send_recv(tx_data)
-        self.assertEqual(self.interface.data, self.framing.encode_frame(tx_data))
+            self.comm.send_recv(tx_data)
+
+    def test_recv_format(self):
+        self.interface.enabled = True
+        tx_data = b'\x01\x02\x03'
+        with self.assertRaises(TypeError):
+            self.comm.send_recv(tx_data, data_format='unknown')
+        with self.assertRaises(CommTimeoutError):
+            self.comm.recv()
 
     def test_send_recv_json(self):
         self.interface.enabled = True
         tx_data = {"str": "test", "obj": {"list": [1, 2, 3], "id": 1}}
-        rx_data = self.comm.send_recv_json(tx_data)
+        rx_data = self.comm.send_recv(tx_data, data_format='json')
+        self.assertEqual(rx_data, tx_data)
+
+    def test_send_recv_string(self):
+        self.interface.enabled = True
+        tx_data = 'test string'
+        rx_data = self.comm.send_recv(tx_data, data_format='string')
         self.assertEqual(rx_data, tx_data)
 
 if __name__ == '__main__':
