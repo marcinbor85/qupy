db4e63874b65020b47679a52e6d24c5026b45638
Marcin Borowicz <marcinbor85@gmail.com>
2020-07-21 23:59:12 +0200

    server tests added

---
+++ tests/test_server.py
@@ -0,0 +1,51 @@
+import time
+import unittest
+
+from qupy.framing.slip import Slip
+
+from qupy.comm.server import CommServer
+from qupy.comm.errors import CommError
+from qupy.interface.errors import InterfaceTimeoutError
+
+from .common import EchoInterface
+
+
+class TestCommServer(unittest.TestCase):
+
+    def setUp(self):
+        self.framing = Slip()
+        self.interface = EchoInterface()
+        self.comm = CommServer(self.interface, self.framing)
+        self.comm.start()
+
+        super().setUp()
+    
+    def tearDown(self):
+        self.comm.stop()
+
+        super().tearDown()
+
+    def test_recv_and_send(self):
+        self.interface.enabled = True
+        tx_msg = b'\x01\x02\x03'
+        tx_data = self.framing.encode_frame(tx_msg)
+        self.interface.write(tx_data)
+        rx_data = self.comm.recv()
+        self.assertEqual(rx_data, tx_msg)
+        tx_msg = b'\x04\x05\x06'
+        tx_data = self.framing.encode_frame(tx_msg)
+        self.interface.enabled = False
+        self.comm.send(tx_msg)
+        rx_data = bytearray()
+        time.sleep(1.0)
+        self.comm.stop()
+        self.interface.enabled = True
+        while True:
+            try:
+                rx_data += self.interface.read()
+            except InterfaceTimeoutError:
+                break
+        self.assertEqual(rx_data, tx_data)
+
+if __name__ == '__main__':
+    unittest.main()
