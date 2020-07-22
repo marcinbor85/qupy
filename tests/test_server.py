7b7274ebcd00e371625f9b144cbed5c345a0f67a
Marcin Borowicz <marcinbor85@gmail.com>
2020-07-22 08:30:42 +0200

    server additional test case

--- tests/test_server.py
+++ tests/test_server.py
@@ -32,9 +32,10 @@ class TestCommServer(unittest.TestCase):
         self.interface.write(tx_data)
         rx_data = self.comm.recv()
         self.assertEqual(rx_data, tx_msg)
+        
+        self.interface.enabled = False
         tx_msg = b'\x04\x05\x06'
         tx_data = self.framing.encode_frame(tx_msg)
-        self.interface.enabled = False
         self.comm.send(tx_msg)
         rx_data = bytearray()
         time.sleep(1.0)
@@ -46,6 +47,26 @@ class TestCommServer(unittest.TestCase):
             except InterfaceTimeoutError:
                 break
         self.assertEqual(rx_data, tx_data)
+    
+    def test_recv_and_no_recv(self):
+        self.interface.enabled = True
+        tx_msg = b'\x01\x02\x03'
+        tx_data = self.framing.encode_frame(tx_msg)
+        self.interface.write(tx_data)
+        rx_data = self.comm.recv()
+        self.assertEqual(rx_data, tx_msg)
+        self.interface.enabled = False
+        self.comm.send(None)
+        rx_data = bytearray()
+        time.sleep(1.0)
+        self.comm.stop()
+        self.interface.enabled = True
+        while True:
+            try:
+                rx_data += self.interface.read()
+            except InterfaceTimeoutError:
+                break
+        self.assertEqual(rx_data, bytearray())
 
 if __name__ == '__main__':
     unittest.main()
