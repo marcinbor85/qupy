219563523c975d1d5ae2aa47bbd02862c906ab43
Marcin Borowicz <marcinbor85@gmail.com>
2020-07-27 21:35:19 +0200

    serial port comm client example added

---
+++ examples/serial_client.py
@@ -0,0 +1,48 @@
+import logging
+import time
+
+from qupy.framing.slip import Slip
+from qupy.interface.serial import SerialPort
+from qupy.interface.errors import InterfaceTimeoutError, InterfaceIOError, InterfaceError
+from qupy.comm.client import CommClient
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+    s = SerialPort()
+    f = Slip()
+
+    c = CommClient(s, f)
+
+    connect = True
+    while True:
+        
+        if connect:
+            try:
+                s.open()
+            except InterfaceIOError as e:
+                time.sleep(1.0)
+                continue
+            c.start()
+            connect = False
+            
+        try:
+            print('ask...')
+            data = input()
+            d = c.ask(data.encode('utf-8'))
+            print('data:',d)
+            if len(d) > 0 and d[0] == ord('p'):
+                
+                break
+        except InterfaceIOError as e:
+            print('ask io error', str(e))
+            c.stop()
+            s.close()
+            connect = True
+        except InterfaceTimeoutError as e:
+            print('timeout')
+
+    c.stop()
+    s.close()
